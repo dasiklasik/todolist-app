a3412364b205a490998180c743e1a49a06eaf018
dasiklasik <daryasamsonowicz@gmail.com>
2023-03-06 21:15:15 +0300

    feat: add auth reducer and setAuth action

---
+++ src/state/authReducer.ts
@@ -0,0 +1,21 @@
+
+const initialState = {
+    isAuth: false,
+}
+
+export const authReducer = (state = initialState, action: actionType) => {
+    switch (action.type) {
+        case 'SET-AUTH':
+            return {...state, isAuth: action.isAuth}
+        default: return state
+    }
+}
+
+const setAuth = (isAuth: boolean) => ({type: 'SET-AUTH', isAuth} as const)
+
+
+type initialStateType = typeof initialState
+
+type setAuthType = ReturnType<typeof setAuth>
+
+type actionType = setAuthType
