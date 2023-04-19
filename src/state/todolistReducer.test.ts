7d2cceb2f9e179465632cd2c5f8dde962d5dc19e
dasiklasik <daryasamsonowicz@gmail.com>
2023-04-19 16:35:39 +0300

    feat: add test for todolistReducer

---
+++ src/state/todolistReducer.test.ts
@@ -0,0 +1,56 @@
+import { v1 } from "uuid";
+import {
+    addTodolist,
+    changeTodolistFilter, changeTodolistTitle,
+    removeTodolist,
+    todolistId1,
+    todolistId2,
+    todolistReducer,
+    TodolistType
+} from "./todolistReducer";
+
+let initialState: Array<TodolistType>
+
+beforeEach(() => {
+    initialState = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'}
+    ]
+})
+
+test('todolist reducer should remove todolist', () => {
+
+    const action = removeTodolist(todolistId1)
+    const endState = todolistReducer(initialState, action)
+
+    expect(endState.length).toBe(1)
+    expect(endState[0].title).toBe('What to buy')
+})
+
+test('todolist reducer should add todolist', () => {
+
+    const id = v1()
+    const action = addTodolist(id, 'new todo')
+    const endState = todolistReducer(initialState, action)
+
+    expect(endState.length).toBe(3)
+    expect(endState[0].id).toBe(id)
+    expect(endState[0].title).toBe('new todo')
+    expect(endState[0].filter).toBe('all')
+})
+
+test('todolist reducer should change todolist title', () => {
+    const action = changeTodolistTitle(todolistId1, 'new todo title')
+    const endState = todolistReducer(initialState, action)
+
+    expect(endState.length).toBe(2)
+    expect(endState[0].title).toBe('new todo title')
+})
+
+test('todolist reducer should change todolist filter', () => {
+    const action = changeTodolistFilter(todolistId1, 'active')
+    const endState = todolistReducer(initialState, action)
+
+    expect(endState.length).toBe(2)
+    expect(endState[0].filter).toBe('active')
+})
